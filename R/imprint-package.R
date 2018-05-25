a261ab233835bee82bb67ce41e41202806c20951
boB Rudis <bob@rud.is>
2018-05-24 22:00:05 -0400

    initial commit

---
+++ R/imprint-package.R
@@ -0,0 +1,11 @@
+#' Create Customized 'ggplot2' and 'R Markdown' Themes for Your Organization
+#'
+#' @md
+#' @name imprint
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import ggplot2 grid scales extrafont
+#' @importFrom magrittr %>%
+#' @import rmarkdown knitr htmltools swatches
+#' @importFrom tools file_path_sans_ext
+NULL
